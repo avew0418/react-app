21319612176a7465a1130e446a1fe766a37081d6
avew0418 <avew.yone@gmail.com>
2025-06-10 18:16:23 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/6-7rt/', // 這裡要改成你的 repository 名稱
+});
