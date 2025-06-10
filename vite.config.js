994a524c9c40038fd101a6722c0fc0f9612b17e6
avew0418 <avew.yone@gmail.com>
2025-06-10 18:26:12 +0800

    更新 Vite 配置中的基礎路徑為 '/react-app/'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/6-7rt/', // 這裡要改成你的 repository 名稱
+  base: '/react-app/', // 這裡要改成你的 repository 名稱
 });
